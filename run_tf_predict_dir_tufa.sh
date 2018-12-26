946093d02be5df917312f722c093543fc7a67546
Sofie <svlandeg@users.noreply.github.com>
2018-12-26 20:23:31 +0100

    batch predictions and try different architecture (#1)

---
+++ run_tf_predict_dir_tufa.sh
@@ -0,0 +1,5 @@
+python src/be/svlandeg/tufa/algo/label_image_dir.py --graph=C:/tmp/output_graph.pb --labels=C:/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --dir=data/tufa_gold/tufa/
+
+python src/be/svlandeg/tufa/algo/label_image_dir.py --graph=C:/tmp/output_graph.pb --labels=C:/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --dir=data/tufa_gold/nontufa/
+
+
