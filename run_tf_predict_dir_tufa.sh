4299a4365e5a2cbf5df81f3a2ea18eb87780f0d1
svlandeg <sofie.vanlandeghem@gmail.com>
2018-12-26 22:49:05 +0100

    faster training and testing with MobileNet instead of Inception V3

--- run_tf_predict_dir_tufa.sh
+++ run_tf_predict_dir_tufa.sh
@@ -1,5 +1,3 @@
-python src/be/svlandeg/tufa/algo/label_image_dir.py --graph=C:/tmp/output_graph.pb --labels=C:/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --dir=data/tufa_gold/tufa/
+python src/be/svlandeg/tufa/algo/label_image_dir.py --graph=C:/tmp/output_graph.pb --labels=C:/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --input_height=224 --input_width=224 --dir=data/tufa_gold/tufa/
 
-python src/be/svlandeg/tufa/algo/label_image_dir.py --graph=C:/tmp/output_graph.pb --labels=C:/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --dir=data/tufa_gold/nontufa/
-
-
+python src/be/svlandeg/tufa/algo/label_image_dir.py --graph=C:/tmp/output_graph.pb --labels=C:/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --input_height=224 --input_width=224 --dir=data/tufa_gold/nontufa/
